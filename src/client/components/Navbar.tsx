e781f0204e15d39bf5ac86e39ccb833bcbfd1f35
unknown <xlanimation@gmail.com>
2021-08-04 09:59:01 -0500

    navbar update

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-let Navbar = () => {
+let Navbar = () =>
+{
     return (
         <>
+            <div className="container">
+                <nav className="navbar navbar-light bg-dark">
+                    <div className="container-fluid">
+                        <div className="logo">
+                            <Link to='/'>
+                                from.<span className="skratch">skratch</span>
+                            </Link>
+                        </div>
+                        <nav>
+                            <ul className="log-user">
+                                <i className="fas fa-user-plus"></i>
+                                <i className="fas fa-sign-in-alt"></i>
+                            </ul>
+                            <ul id="nav" className="nav">
+                                <li><a className="nav-link" href="">Home</a></li>
+                                <li><a className="nav-link" href="">Recipes</a></li>
+                                <li><a className="nav-link" href="">Submit</a></li>
+                                <li><a className="nav-link" href="">About</a></li>
+                                <li><a className="nav-link" href="">Contact</a></li>
+                            </ul>
+                        </nav>
+                        {/*<div className="logo">
+                            <LogoLink to='/'>
+                                from.<span className="skratch">skratch</span>
+                            </LogoLink>
+                        </div>
+                         <div className="logo">
+                            <Link to="/">
+                                <span className="from">from.</span><span className="skratch">skratch</span>
+                            </Link>
+                        </div> */}
+                    </div>
+                </nav>
+            </div>
 
-            <h1>From Skratch</h1>
-
-            <Link to="/about" >About</Link>
 
         </>
 
