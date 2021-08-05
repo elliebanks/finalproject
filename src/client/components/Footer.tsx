ada3c55074a8d7db78a806ca2f37cec4e33129e8
unknown <xlanimation@gmail.com>
2021-08-05 09:22:38 -0500

    nav/footer updates

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,24 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // import fa-icons
 
 let Footer = () =>
 {
     return (
         <>
             <footer>
-                <div className="container bg-dark">
-                    <div className="row">
-                        <div className="col">
-                            Footer Section One
+                <div className="row">
+                    <div className="container d-flex justify-content-center">
+                        <div className="footCall">
+                            <h1>You're Still Here!</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum molestie lorem id tincidunt. Aliquam ut tristique purus.</p>
+                            <Link to="/submit">
+                                Submit your Recipe!
+                            </Link>
                         </div>
-                        <div className="col">
-                            Footer Section Two
-                        </div>
-                        <div className="col">
-                            Footer Section Three
+                        <div className="footImg">
+                            <img src="/images/footCall.png" alt="image" />
                         </div>
                     </div>
-
+                </div>
+                <div className="row bg-info">
+                    <div className="container bg-success">
+                        <div className="row">
+                            <div className="col">
+                                <div className="logo">
+                                    <Link to="/">
+                                        from.<span className="logo">skratch</span>
+                                    </Link>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum molestie lorem id tincidunt. Aliquam ut tristique purus.</p>
+                                    <FontAwesomeIcon icon={['fab', 'html5']} />
+                                    <FontAwesomeIcon icon={['fab', 'react']} />
+                                    <FontAwesomeIcon icon={['fab', 'css3-alt']} />
+                                    <FontAwesomeIcon icon={['fab', 'js-square']} />
+                                    <FontAwesomeIcon icon={['fab', 'node-js']} />
+                                    <FontAwesomeIcon icon={['fab', 'font-awesome-flag']} />
+                                </div>
+                            </div>
+                            <div className="col">
+                                <h3>Follow Us!</h3>
+                                <FontAwesomeIcon icon={['fab', 'facebook']} />
+                                <FontAwesomeIcon icon={['fab', 'twitter']} />
+                                <FontAwesomeIcon icon={['fab', 'youtube']} />
+                                <FontAwesomeIcon icon={['fab', 'linkedin']} />
+                            </div>
+                            <div className="col">
+                                <h3>Sitemap</h3>
+                                <ul>
+                                    <li>
+                                        <Link to="/" >
+                                            <FontAwesomeIcon icon="home" />
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/recipes">
+                                            <FontAwesomeIcon icon="utensils" />
+                                            Recipes
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/submit">
+                                            <FontAwesomeIcon icon="file-alt" />
+                                            Submit
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/about" >
+                                            <FontAwesomeIcon icon="question-circle" />
+                                            About
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/contact">
+                                            <FontAwesomeIcon icon="address-book" />
+                                            Contact
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="container text-center">
+                                copyright info
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </footer>
         </>
--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -1,53 +1,11 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-import styled from "styled-components";
-=======
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // import fa-icons
->>>>>>> navbar
 
 let Navbar = () =>
 {
     return (
         <>
-<<<<<<< HEAD
-            <div className="container">
-                <nav className="navbar navbar-light bg-dark">
-                    <div className="container-fluid">
-                        <div className="logo">
-                            <Link to='/'>
-                                from.<span className="skratch">skratch</span>
-                            </Link>
-                        </div>
-                        <nav>
-                            <ul className="log-user">
-                                <i className="fas fa-user-plus"></i>
-                                <i className="fas fa-sign-in-alt"></i>
-                            </ul>
-                            <ul id="nav" className="nav">
-                                <li><a className="nav-link" href="">Home</a></li>
-                                <li><a className="nav-link" href="">Recipes</a></li>
-                                <li><a className="nav-link" href="">Submit</a></li>
-                                <li><a className="nav-link" href="">About</a></li>
-                                <li><a className="nav-link" href="">Contact</a></li>
-                            </ul>
-                        </nav>
-                        {/*<div className="logo">
-                            <LogoLink to='/'>
-                                from.<span className="skratch">skratch</span>
-                            </LogoLink>
-                        </div>
-                         <div className="logo">
-                            <Link to="/">
-                                <span className="from">from.</span><span className="skratch">skratch</span>
-                            </Link>
-                        </div> */}
-                    </div>
-                </nav>
-            </div>
-
-
-=======
             {/* Start Header */}
             <header>
                 {/* Wrapper Start */}
@@ -98,7 +56,6 @@ let Navbar = () =>
                 </div>
             </header>
             {/* End Header */}
->>>>>>> navbar
         </>
 
     )
