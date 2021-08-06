27dbc30ff02bbc387f78c8711ecfeedb947ef32c
unknown <xlanimation@gmail.com>
2021-08-06 17:22:50 -0500

    ui/ux update sitewide

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,77 +11,97 @@ let About = () =>
                 <img src="/images/about2.jpg" className="img-fluid" alt="from-skratch-logo" id="aboutPageLogo" />
             </div> */}
 
-            <div className="jumbotron jumbotron-fluid mb-5">
-                <div className="container">
-                    <h1>About Us</h1>
-                    <p className="lead">About from.skratch and the development team.</p>
+            <div id="backDiv" className="bgColor"></div>
+
+            <div className="container">
+                <div className="row">
+                    {/* Left-Column */}
+                    <div className="col-8 leftColumn">
+                        <section>
+                            <div id="sectionTitle" className="secTitle">
+                                <h1>About Us</h1>
+                                <h6>About from.skratch and the development team.</h6>
+                            </div>
+                            <div className="secContent">
+                                <h3>
+                                    Hi! We are the "from.Skratch" Team
+                                </h3>
+                                <p>
+                                    We are a group of 5 web-developers who wanted to create a community for our love of good FOOD.
+                                    Don't you hate the days when you are searching through your pantry and just don't know what to make?
+                                    So do we! From.Skratch is here to help you make those decisions, while making it as easy as possible in the process.
+                                </p>
+                                <p>
+                                    Whether you've got some ingredients you aren't sure what to do with or maybe you are a beginner looking for some tasty new recipe ideas...From.Skratch has your back!
+                                    Pop those random ingredients in our search bar, and explore your options.
+                                </p>
+                                <p>
+                                    Have you been making recipes that you love and want to share them with the world? Head to our recipe form and let us know what you've got.
+                                    We've got hundreds of original recipes. Check out our recipes page if you just want to see what your friends are cooking!
+                                    We are a community of food lovers, passionate about connecting with the foodie in you. Come on and stay a while!
+                                </p>
+                            </div>
+                        </section>
+                    </div>
+
+                    {/* Right-Column */}
+                    <div className="col-4 rightColumn">
+                        <div className="secConHead">
+                            <img className="aboutHeadImg" src="/images/aboutHead.jpg" alt="image" />
+                        </div>
+                    </div>
+
                 </div>
-            </div>
-
-            <div className="d-flex justify-content-center mb-3">
-                <h3 className="" id="aboutIntroHeader">
-                    Hi! We are the "from.Skratch" Team</h3>
-            </div>
-
-            <div className="container d-flex justify-content-center w-50">
-                <p className="display-6" id="aboutIntroText">
-                    We are a group of 5 web-developers who wanted to create a community for our love of good FOOD.
-                    Don't you hate the days when you are searching through your pantry and just don't know what to make?
-                    So do we! From.Skratch is here to help you make those decisions, while making it as easy as possible in the process.
-
-                    Whether you've got some ingredients you aren't sure what to do with or maybe you are a beginner looking for some tasty new recipe ideas...From.Skratch has your back!
-                    Pop those random ingredients in our search bar, and explore your options.
-                    Have you been making recipes that you love and want to share them with the world? Head to our recipe form and let us know what you've got.
-                    We've got hundreds of original recipes. Check out our recipes page if you just want to see what your friends are cooking!
-                    We are a community of food lovers, passionate about connecting with the foodie in you. Come on and stay a while!
-
-                </p>
             </div>
 
             <div className="container aboutTeam">
-                <div className="card-columns">
-                    <div className="card">
-                        <img className="card-img-top" src=".../100px160/" alt="Card image cap" />
-                        <div className="card-body">
-                            <h3 className="card-title">Briana</h3>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <div className="row">
+                    <div id="sectionTitle" className="col secTitle">
+                        <h1>About Us</h1>
+                        <h6>About from.skratch and the development team.</h6>
+                    </div>
+                    <div className="card-columns">
+                        <div className="card">
+                            <img className="card-img-top" src=".../100px160/" alt="Card image cap" />
+                            <div className="card-body">
+                                <h3 className="card-title">Briana</h3>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <img className="card-img-top" src=".../100px160/" alt="Card image cap" />
+                            <div className="card-body">
+                                <h3 className="card-title">Moshe</h3>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <img className="card-img-top" src=".../100px160/" alt="Card image cap" />
+                            <div className="card-body">
+                                <h3 className="card-title">Justin</h3>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <img className="card-img-top" src="/images/ellie.png" alt="Card image cap" />
+                            <div className="card-body">
+                                <h3 className="card-title">Ellie</h3>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <img className="card-img-top" src="/images/marques.jpg" alt="Card image cap" />
+                            <div className="card-body">
+                                <h3 className="card-title">Marques</h3>
+                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
                         </div>
                     </div>
-
-                    <div className="card">
-                        <img className="card-img-top" src=".../100px160/" alt="Card image cap" />
-                        <div className="card-body">
-                            <h3 className="card-title">Moshe</h3>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <img className="card-img-top" src=".../100px160/" alt="Card image cap" />
-                        <div className="card-body">
-                            <h3 className="card-title">Justin</h3>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <img className="card-img-top" src="/images/ellie.png" alt="Card image cap" />
-                        <div className="card-body">
-                            <h3 className="card-title">Ellie</h3>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-
-                    <div className="card">
-                        <img className="card-img-top" src="/images/marques.jpg" alt="Card image cap" />
-                        <div className="card-body">
-                            <h3 className="card-title">Marques</h3>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-
                 </div>
-
             </div>
 
 
--- src/client/pages/RecipeSearch.tsx
+++ src/client/pages/RecipeSearch.tsx
@@ -4,11 +4,13 @@ import { faUtensils, faStopwatch } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-const RecipeSearch = () => {
+const RecipeSearch = () =>
+{
 
     const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
+    useEffect(() =>
+    {
         fetch('http://localhost:3000/api/recipes')
             .then(res => res.json())
             .then(recipes => setRecipes(recipes))
@@ -17,38 +19,56 @@ const RecipeSearch = () => {
     return (
 
         <>
-            <div>
-                <h2 className="d-flex justify-content-center">Recipes</h2>
-            </div>
-            {/* First row of cards */}
-            <div className="container mb-5" id="recipeCardContainer">
-                <div className="row d-flex justify-content-center">
+            <div id="recipes" className="bgColor"></div>
+
+            <div className="container mb-5">
+                <div className="row no-gutters">
+                    <div className="container">
+                        <div id="sectionTitle" className="col secTitle text-center">
+                            <h1>Recipes</h1>
+                            <h6>The best recipes for you, submitted by people just like you!</h6>
+                        </div>
+                    </div>
+
+                    {/* component start */}
 
                     {recipes.map(recipe => (
-                        <div className="col-sm-4 card w-25 m-5" id="recipeCard" key={recipe.id}>
-                            <img src="https://diestelturkey.com/wp-content/uploads/2019/04/DFR-Italian-Hot-Turkey-Sausage-Link-lifestyle-600x600.jpg" className="card-img-top" alt="{recipe.title}" />
-                            <div className="card-body mb-1">
-                                <h4>{recipe.title}</h4>
+                        <div className="col-4 bg-info" id="recipeCard" key={recipe.id}>
+                            <div className="recipeCardWrap">
+                                <img src="https://diestelturkey.com/wp-content/uploads/2019/04/DFR-Italian-Hot-Turkey-Sausage-Link-lifestyle-600x600.jpg" className="card-img-top" alt="{recipe.title}" />
+                                <div className="card-body mb-1">
+                                    <h4>{recipe.title}</h4>
 
-                                <div className="" id="servingDetails">
-                                    <span className="d-flex inline">
-                                        <FontAwesomeIcon className="m-1" icon={faUtensils} />
-                                        <p className="servingSize"> Serving Size:  {recipe.servings}  </p> </span>
-                                </div>
-                                <div className="" id="cookingTimeDetails">
-                                    <span className="d-flex inline">
-                                        <FontAwesomeIcon className="m-1" icon={faStopwatch} />
-                                        <p> Cooking Time:  {recipe.cooktime}</p>
-                                    </span>
-                                </div>
+                                    <div className="" id="servingDetails">
+                                        <span className="d-flex inline">
+                                            <FontAwesomeIcon className="m-1" icon={faUtensils} />
+                                            <p className="servingSize"> Serving Size:  {recipe.servings}  </p> </span>
+                                    </div>
+                                    <div className="" id="cookingTimeDetails">
+                                        <span className="d-flex inline">
+                                            <FontAwesomeIcon className="m-1" icon={faStopwatch} />
+                                            <p> Cooking Time:  {recipe.cooktime}</p>
+                                        </span>
+                                    </div>
 
-                                <p className="card-text d-flex justify-content-center" id="recipeDescription">{recipe.description}</p>
-                                <button>Link Placeholder</button>
+                                    <p className="card-text d-flex justify-content-center" id="recipeDescription">{recipe.description}</p>
+                                    <button>Link Placeholder</button>
+                                </div>
                             </div>
                         </div>
-                    ))}
+                    ))};
+
+                    {/* component end */}
+
                 </div>
             </div>
+
+            {/* <div className="container mb-5" id="recipeCardContainer">
+                <div className="row d-flex justify-content-center">
+
+                    
+                </div>
+            </div> */}
 
         </>
 
